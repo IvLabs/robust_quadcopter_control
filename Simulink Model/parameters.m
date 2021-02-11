7533560775b293189196c8c19fcc5a8203fc3db4
RiteshZoad <64415087+RiteshZoad@users.noreply.github.com>
2021-02-11 19:06:58 +0530

    Added Simulink Model

---
+++ Simulink Model/parameters.m
@@ -0,0 +1,8 @@
+m = 0.18; % kg
+g = 9.81; % m/s/s
+Kd =    0.08         % drag coefficient
+d =    0.25          % dist betn quadrotor com and rotation axis of propeller
+ 
+Ix = 0.00025
+Iy = 0.000432
+Iz = 0.0003738
